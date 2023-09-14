7e5569e9b5e4523326d1f8c3597c9b7d1cda8b66
eds9nsilva <antonioedson99@gmail.com>
2023-09-13 23:09:29 -0300

    feat: TextInput component

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {SafeAreaView, View} from 'react-native';
 import {Button} from './src/components/Button/Button';
 import {Icon} from './src/components/Icons/Icon';
 import {Text} from './src/components/Text/Text';
+import {TextInput} from './src/components/TextInput/TextInput';
 import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
@@ -11,12 +12,29 @@ function App(): JSX.Element {
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge">Edson</Text>
-          <Button title="Entrar" preset="primary" disabled marginBottom="s24" />
-          <Button title="Entrar" preset="outline" disabled marginBottom="s24" />
-          <Button title="Entrar" preset="secondary" marginBottom="s24" />
-          <Icon name="eyeOn" color="error" size={40} />
-          <Button title="Entrar" />
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <TextInput
+            label="Email"
+            placeholder="Digite seu e-mail"
+            errorMessage="Mensagem de error"
+            boxProps={{mb: 's20'}}
+          />
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            RightComponent={<Icon name="eyeOn" />}
+            boxProps={{mb: 's10'}}
+          />
+          <Text color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
+          </Text>
+          <Button title="Entrar" mt="s48" />
+          <Button preset="outline" title="Criar uma conta" mt="s12" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
