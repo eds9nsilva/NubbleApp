a4ab561ee1704ec8d4e89cba0dc3c908b62ca9c3
eds9nsilva <antonioedson99@gmail.com>
2023-10-13 00:26:18 -0300

    feat: FormControll

--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 import {Button} from '../../../components/Button/Button';
-import {PasswordInput} from '../../../components/PasswordInput/PassowrdInput';
+import {FormPasswordInput} from '../../../components/Form/FormPasswordInput';
+import {FormTextInput} from '../../../components/Form/FormtextInput';
 import {Screen} from '../../../components/Screen/Screen';
 import {Text} from '../../../components/Text/Text';
-import {TextInput} from '../../../components/TextInput/TextInput';
 import {useResetNavigationSuccess} from '../../../hooks/useResetNavigationSuccess';
+
+type SignUpFormType = {
+  username: string;
+  fullname: string;
+  email: string;
+  password: string;
+};
 
 export function SignUpScreen() {
   const {reset} = useResetNavigationSuccess();
+  const {control, formState, handleSubmit} = useForm<SignUpFormType>({
+    defaultValues: {
+      email: '',
+      fullname: '',
+      password: '',
+      username: '',
+    },
+    mode: 'onChange',
+  });
+
   function submitForm() {
     reset({
       title: 'Sua conta foi criada com sucesso!',
@@ -23,24 +41,50 @@ export function SignUpScreen() {
       <Text preset="headingLarge" mb="s32">
         Criar uma conta
       </Text>
-      <TextInput label="Seu username" placeholder="@" boxProps={{mb: 's20'}} />
-      <TextInput
-        label="Nome Completo"
+      <FormTextInput
+        control={control}
+        name="username"
+        rules={{required: 'Username obrigatório'}}
+        label="Seu username"
+        placeholder="@"
+        boxProps={{mb: 's20'}}
+      />
+      <FormTextInput
+        control={control}
+        name="fullname"
+        rules={{required: 'Nome obrigatório'}}
+        label="Nome completo"
         placeholder="Digite seu nome completo"
         boxProps={{mb: 's20'}}
       />
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="email"
+        rules={{required: 'E-mail obrigatório'}}
         label="E-mail"
-        placeholder="Digite seu e-mail"
+        placeholder="Digite seu E-mail"
         boxProps={{mb: 's20'}}
       />
-
-      <PasswordInput
+      <FormPasswordInput
+        control={control}
+        name="password"
+        rules={{
+          required: 'Senha obrigatória',
+          minLength: {
+            value: 8,
+            message: 'Senha deve ter no mínimo 8 caracteres',
+          },
+        }}
         label="Senha"
         placeholder="Digite sua senha"
         boxProps={{mb: 's20'}}
       />
-      <Button onPress={submitForm} title="Criar uma conta" />
+
+      <Button
+        onPress={handleSubmit(submitForm)}
+        disabled={!formState.isValid}
+        title="Criar uma conta"
+      />
     </Screen>
   );
 }
