ad04756c054cdc3098c7cd95b3ef090cd64fbd4e
@edson.ferreira <edson.ferreira@fortics.com.br>
2023-11-30 23:24:59 -0300

    feat: camada de infra

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,11 +1,20 @@
-import { postListMock } from "./postListMock";
-import { Post } from "./types";
+import {PageAPI} from '@api';
 
-async function getList(): Promise<Post[]> {
-    await new Promise(resolve => setTimeout(() => resolve(''), 1000))
-    return postListMock;
+import {PostAPI} from './postTypes';
+
+async function getList(): Promise<PageAPI<PostAPI>> {
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer Mg.R5wy9_TgoQXaBCRrhnJhuOKaDZhvFqFnhSrnI0xYsOMHJfLC6lj45lCaC-7d',
+    },
+  });
+
+  let data: PageAPI<PostAPI> = await response.json();
+  return data;
 }
 
 export const postApi = {
-    getList,
-}
+  getList,
+};
