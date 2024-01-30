070474db0a5b1e78fc3d845b9902a09f1b051dcf
@edson.ferreira <edson.ferreira@fortics.com.br>
2024-01-29 22:49:36 -0300

    feat: scroll to top

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { PostItem, Screen, } from '@components';
 import { AppTabScreenProps } from '@routes';
@@ -6,9 +6,12 @@ import { Post, usePostList } from '@domain';
 import { FlatList, ListRenderItemInfo, RefreshControl, StyleProp, ViewStyle } from 'react-native';
 import { HomeHeader } from './Components/HomeScreen';
 import { HomeEmpty } from './Components/HomeEmpty';
+import { useScrollToTop } from '@react-navigation/native';
 
 export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
   const { error, loading, postList, refresh, fetchNextPage } = usePostList();
+  const flatListRef = useRef<FlatList<Post>>(null);
+  useScrollToTop(flatListRef);
 
   function renderItem({ item }: ListRenderItemInfo<Post>) {
     return (
@@ -19,17 +22,22 @@ export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
   return (
     <Screen style={$screen}>
       <FlatList
+        ref={flatListRef}
         showsVerticalScrollIndicator={false}
         data={postList}
-        keyExtractor={(item) => item.id}
+        keyExtractor={item => item.id}
         renderItem={renderItem}
-        onEndReachedThreshold={0.1}
         onEndReached={fetchNextPage}
+        onEndReachedThreshold={0.1}
         refreshing={loading}
-        refreshControl={<RefreshControl refreshing={loading} onRefresh={refresh} />}
-        contentContainerStyle={{ flex: postList.length == 0 ? 1 : undefined }}
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={refresh} />
+        }
+        contentContainerStyle={{flex: postList.length === 0 ? 1 : undefined}}
         ListHeaderComponent={<HomeHeader />}
-        ListEmptyComponent={<HomeEmpty refetch={refresh} error={error} loading={loading} />}
+        ListEmptyComponent={
+          <HomeEmpty refetch={refresh} error={error} loading={loading} />
+        }
       />
     </Screen>
   );
