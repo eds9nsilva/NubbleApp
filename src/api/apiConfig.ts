db0cf94bd5b0056ce55e5c84abdca626b97d669f
@edson.ferreira <edson.ferreira@fortics.com.br>
2024-01-30 23:43:41 -0300

    feat: postComment

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
     baseURL: 'http://127.0.0.1:3333/',
     timeout: 1200,
     headers: {
-        Authorization: 'Bearer NA.jfirVTQkdrSV-tz2saUJfxKRRV70Vzu0m2faIsE2IrwoUwY_nJaHEi6Y9NLN',
+        Authorization: 'Bearer MQ.MrCJ6Oc0IoO_3FKeAnCVsHXyN1GPBLyEHaAdygcw-OYcZyToytXipy8HduES',
     }
 })
